ef8e167dff6cfa4a8fa894d5bd437b8320f2feb1
Nasimul-Rahman <nasimul@cynergon.net>
2023-04-10 15:38:16 +0600

    intro animation added

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,18 @@ console.log("hello");
 
 var loader = document.getElementById("preloader");
 
+// window.addEventListener('load', function() {
+//     loader.style.display = "none"
+// })
+
+
+let intro = document.querySelector('.intro')
+let logo = document.querySelector('.logo_animation')
+
 window.addEventListener('load', function() {
-    loader.style.display = "none"
+    setTimeout(myURL, 2500);
 })
+
+function myURL() {
+    intro.style.opacity = 0;
+ }
