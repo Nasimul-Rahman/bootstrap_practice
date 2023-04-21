5de9fb9cf694c7b93ffcfd9730f96308e955a31b
Nasimul-Rahman <nasimul@cynergon.net>
2023-04-21 10:15:53 +0600

    gallery section added

--- JS/gallery.js
+++ JS/gallery.js
@@ -3,13 +3,17 @@ console.log("gallery.js is working");
 const imageModal = document.querySelectorAll('.image_modal');
 const imageModalClose = document.querySelectorAll('.image_modal_close');
 const imageContainers = document.querySelectorAll('.image_gallery_card');
+const section = document.querySelectorAll('section');
 
-console.log(imageModal);
+console.log(section);
 
 for (let i = 0; i < imageContainers.length; i++) {
     console.log(imageContainers[i]);
     imageContainers[i].addEventListener("click", function() {
         imageModal[i].style.display = 'flex'
+        for (let j = 0; j < section.length; j++) {
+            section[j].classList.remove('.hidden')
+        }
     })
 }
 
@@ -17,5 +21,8 @@ for (let i = 0; i < imageModalClose.length; i++) {
     console.log(imageModalClose[i]);
     imageModalClose[i].addEventListener("click", function() {
         imageModal[i].style.display = 'none'
+        for (let j = 0; j < section.length; j++) {
+            section[j].classList.add('.hidden')
+        }
     })
 }
