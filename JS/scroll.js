eb8ca8fc7268b83ea62aeec5d30b19bfd80ef1d4
Nasimul-Rahman <nasimul@cynergon.net>
2023-12-19 10:06:22 +0600

    Desktop view updated and animations added

--- JS/scroll.js
+++ JS/scroll.js
@@ -5,8 +5,9 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
+            entry.target.classList.remove('hidden')
+            entry.target.classList.remove('hidden_general_description')
+            entry.target.classList.remove('hidden_product_card')
         }
     })
 })
