0d5fa9d0462b1152c5282c571730a599ccd5e36f
Nasimul-Rahman <nasimul@cynergon.net>
2023-12-19 11:43:53 +0600

    More updates to gallery

--- JS/gallery.js
+++ JS/gallery.js
@@ -11,6 +11,9 @@ for (let i = 0; i < imageContainers.length; i++) {
     console.log(imageContainers[i]);
     imageContainers[i].addEventListener("click", function() {
         imageModal[i].style.display = 'flex'
+        imageModal[i].style.opacity = '1';
+        imageModal[i].classList.remove('fadeOut')
+        imageModal[i].classList.add('fadeIn')
         for (let j = 0; j < section.length; j++) {
             section[j].classList.remove('.hidden')
         }
@@ -20,7 +23,12 @@ for (let i = 0; i < imageContainers.length; i++) {
 for (let i = 0; i < imageModalClose.length; i++) {
     console.log(imageModalClose[i]);
     imageModalClose[i].addEventListener("click", function() {
-        imageModal[i].style.display = 'none'
+        setTimeout(function() {
+            imageModal[i].style.opacity = '0';
+            imageModal[i].style.display = "none";
+            imageModal[i].classList.remove('fadeIn')
+          }, 1000)
+          imageModal[i].classList.add('fadeOut')
         for (let j = 0; j < section.length; j++) {
             section[j].classList.add('.hidden')
         }
